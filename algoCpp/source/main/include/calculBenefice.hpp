7c8e229354963ef05662ca56f6459cc1907dacc4
ezekielarakotoarijaona <ezekrako@gmail.com>
2020-04-07 14:53:21 +0200

    add calculBenefice.hpp https://github.com/EzekielaRakotoarijaona/OlapMod/issues/21

---
+++ algoCpp/source/main/include/calculBenefice.hpp
@@ -0,0 +1,20 @@
+//
+//  calculBenefice.hpp
+//  
+//
+//  Created by Ezekiela Rakotoarijaona on 07/04/2020.
+//
+
+#ifndef calculBenefice_hpp
+#define calculBenefice_hpp
+
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+int calculBenefice(vector<int> taillesRequetes, vector<int> requetesMaterialisees, int numeroRequete);
+
+
+#endif /* calculBenefice_hpp */
+
