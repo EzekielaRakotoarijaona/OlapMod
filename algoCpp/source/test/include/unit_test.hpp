6a5ad32d665749dbf7c394608a7560cc8623fe26
cehochet <claire-elise.hochet@etu.u-bordeaux.fr>
2020-04-15 11:23:01 +0200

    Création et initialisation de la table de fait

--- algoCpp/source/test/include/unit_test.hpp
+++ algoCpp/source/test/include/unit_test.hpp
@@ -11,6 +11,16 @@ static const vector<int> taillesRequetes = {0, 3, 5, 6, 7, 6, 7, 8, 1};
 static vector<int> requetesMaterialisees = {7};
 static const vector<int> taillesRequetes2 = {0, 20, 15, 50, 30, 40, 75, 100, 1};
 
+static const vector<vector<int>> tableFait = { {1,4,7},
+                                               {1,5,8},
+                                               {1,4,7},
+                                               {1,5,8},
+                                               {1,6,9},
+                                               {2,6,9},
+                                               {2,4,9},
+                                               {2,4,8},
+                                               {3,5,8},
+                                               {3,6,7},};
 
 bool testCalculBenefice();
 bool testRequeteDep();
