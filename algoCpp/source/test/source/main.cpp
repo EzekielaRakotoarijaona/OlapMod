a45b3caa8a509d2464386005767a8ea476796b15
cehochet <claire-elise.hochet@etu.u-bordeaux.fr>
2020-04-16 11:47:56 +0200

    Tâche #27 : Lecture fichier CSV (format entier) + intégration des données dans un vecteur de vecteur

--- algoCpp/source/test/source/main.cpp
+++ algoCpp/source/test/source/main.cpp
@@ -3,12 +3,49 @@
 #include "../include/unit_test.hpp"//c'est pour l'affichage
 #include <vector>
 #include <map>
+#include <fstream>
+using namespace std;
 
+static const string FICHIER_CSV="table_fait.csv";
 
+    
 
 int main() {
-    
-    printf("Running tests \n");
+
+
+     string ligne;
+    ifstream file (FICHIER_CSV);
+    vector<string> V2;
+    vector<vector<string>> tableFait;
+    getline(file,ligne); //on a pas besoin de la première ligne
+    char delim = ','; // Ddefine the delimiter to split by
+
+    if (file.good())
+    {
+        while (getline(file,ligne,delim))
+        {
+            V2.push_back(ligne);
+            tableFait.push_back(V2);
+        }
+    }
+    else
+        cout<<"erreur d'ouverture du fichier"<<endl;
+
+    /*for(auto it = V2.begin(); it != V2.end(); it++) {
+          cout << *it<<" ";
+
+       }*/
+
+    for (int i = 0; i < tableFait.size(); i++) {
+        for (int j = 0; j < tableFait[i].size(); j++)
+            cout << tableFait[i][j] << " ";
+        cout << endl;
+}
+
+
+
+ /*   
+ printf("Running tests \n");
     if (testCalculBenefice()) {
         printf("Test réussi! \n");
     }
@@ -32,5 +69,7 @@ int main() {
     }
     printf("Test réussi! \n");
     return 0;
-    
+
+*/
+
 }
